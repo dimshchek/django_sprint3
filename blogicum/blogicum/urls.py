b02e4c3af6a7928a02f1b8eeb7a402e7b250d12b
Дмитрий Щеклеин <dim.shchek@yandex.ru>
2024-07-16 10:34:45 +0300

    final

---
+++ blogicum/blogicum/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import include, path
+"""Module Django"""
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('blog.urls', namespace='blog')),
+    path('pages/', include('pages.urls', namespace='pages')),
+]

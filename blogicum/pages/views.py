d66a49ac4df993742e776c8ca9a80f520fd94b7e
Дмитрий Щеклеин <dim.shchek@yandex.ru>
2024-07-16 10:48:34 +0300

    Final 2

--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render
 
 
 def about(request):
+    """Страница about."""
     template = 'pages/about.html'
     return render(request, template)
 
+
 def rules(request):
+    """Страница Правила."""
     template = 'pages/rules.html'
     return render(request, template)
